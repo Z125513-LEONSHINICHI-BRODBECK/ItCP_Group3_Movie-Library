bdb3c38639d2d81ba4222bc56a10699a22dc2fcd
Z125513-LEONSHINICHI-BRODBECK <Z125513@shibaura-it.ac.jp>
2026-01-07 11:49:24 +0900

    Create movie.h

---
+++ movie.h
@@ -0,0 +1,34 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+
+typedef struct Movie Movie;
+
+/* Creation / destruction */
+Movie *movie_create(
+    int id,
+    int release_year,
+    const char *title,
+    const char *origin,
+    const char *genre,
+    const char *director
+);
+
+void movie_destroy(Movie *movie);
+
+/* Getters */
+int movie_get_id(const Movie *movie);
+int movie_get_release_year(const Movie *movie);
+const char *movie_get_title(const Movie *movie);
+const char *movie_get_origin(const Movie *movie);
+const char *movie_get_genre(const Movie *movie);
+const char *movie_get_director(const Movie *movie);
+
+/* Setters */
+void movie_set_id(Movie *movie, int id);
+void movie_set_release_year(Movie *movie, int release_year);
+void movie_set_title(Movie *movie, const char *title);
+void movie_set_origin(Movie *movie, const char *origin);
+void movie_set_genre(Movie *movie, const char *genre);
+void movie_set_director(Movie *movie, const char *director);
+
+#endif /* MOVIE_H */
