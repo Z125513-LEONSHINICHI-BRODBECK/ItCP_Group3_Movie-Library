e551121a8cbb1edca2949ec43eb4b453098334d7
AM25103-PRANAV-KAPPAL <am25103@sic.shibaura-it.ac.jp>
2026-01-21 11:40:01 +0900

    Refactor file_io.h header guard and formatting

--- src/file_io.h
+++ src/file_io.h
@@ -4,12 +4,9 @@
 #include <stdio.h>
 #include "movie_list.h"
 
-/* Load movies from CSV file into an existing MovieList.
-   Returns number of movies loaded, or -1 on fatal error. */
 int load_movies(const char *filename, MovieList *list);
 
-/* Save movies from a MovieList to CSV file.
-   Returns 0 on success, -1 on failure. */
 int save_movies(const char *filename, const MovieList *list);
 
-#endif /* FILE_IO_H */
+#endif
+
