dd4062a1c3c459a6b5a69a7dee93ae262d10a354
AM25103-PRANAV-KAPPAL <am25103@sic.shibaura-it.ac.jp>
2026-01-21 11:37:07 +0900

    Remove comments from movie add/edit header file
    Removed comments describing the add and edit movie functions.

--- src/commands/movie_add_edit.h
+++ src/commands/movie_add_edit.h
@@ -3,16 +3,10 @@
 
 #include "./../movie_list.h"
 
-/*
- * Prompts the user for movie data (ID, release year, title, origin,
- * genre, director), creates a Movie, and adds it to the given MovieList.
- */
+
 void add_movie_manually(MovieList *list);
 
-/*
- * Prompts the user based on the id number passed, to edit the data of
- * movie. Updates the movie with the passed
- */
+
  void edit_movie_manually(MovieList *list, int id);
 
-#endif /* MOVIE_ADD_EDIT_H */
+#endif 
